894dae328fba411af44a5f150137a44a352c26a8
Venkata Krishna <venkata.logtokris@gmail.com>
2021-12-07 00:15:22 +0530

    Updated class to search for pluralsight keyword in google.com and then go to Images tab and select first image.

--- src/main/java/com/examples/WebDriverDemo.java
+++ src/main/java/com/examples/WebDriverDemo.java
@@ -1,11 +1,27 @@
 package com.examples;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverDemo {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.pluralsight.com");
+		//driver.get("http://www.pluralsight.com");
+		driver.get("http://www.google.com");
+		WebElement searchField = driver.findElement(By.name("q"));
+		searchField.click();
+		searchField.sendKeys("pluralsight");
+		searchField.sendKeys(Keys.ENTER);
+		
+		WebElement imagesLink = driver.findElements(By.linkText("Images")).get(0);
+		imagesLink.click();
+		
+		WebElement imagesElement = driver.findElements(By.cssSelector("img[class='rg_i Q4LuWd']")).get(0);
+		imagesElement.click();
+		
+		driver.getPageSource().contains("");
 	}
 }
